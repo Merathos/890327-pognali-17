80a886e4f4425ef479449e1eeaffd707347a9f7f
Vladimir Karetnikov <karetnikov.v.e@gmail.com>
2019-08-06 20:15:32 +0300

    retina done

--- source/js/script.js
+++ source/js/script.js
@@ -1,46 +1,54 @@
 //form
 
-// const dropdown_li = document.querySelector(".countries-select__dropdown--choose");
-// const dropdown_div = document.querySelector(".countries-select__modal-wrapper");
+const dropdown_li = document.querySelector(".countries-select__dropdown--choose");
+const dropdown_div = document.querySelector(".countries-select__modal-wrapper");
 
-// dropdown_li.addEventListener('click', evt => {
-//   evt.preventDefault();
-//   dropdown_li.classList.toggle("countries-select__dropdown--active");
-//   dropdown_div.classList.toggle("countries-select__modal-wrapper--show");
-// });
-
+if (dropdown_li) {
+  dropdown_li.addEventListener('click', evt => {
+    evt.preventDefault();
+    dropdown_li.classList.toggle("countries-select__dropdown--active");
+    dropdown_div.classList.toggle("countries-select__modal-wrapper--show");
+  });
+}
 //catalog
 
-// const open_btn = document.querySelector(".filter__open-btn");
-// const top_wrapper = document.querySelector(".filter__top-wrapper");
-// const bottom_wrapper = document.querySelector(".filter__bottom-wrapper");
+const open_btn = document.querySelector(".filter__open-btn");
+const top_wrapper = document.querySelector(".filter__top-wrapper");
+const bottom_wrapper = document.querySelector(".filter__bottom-wrapper");
 
-// open_btn.addEventListener('click', evt => {
-//   evt.preventDefault();
-//   top_wrapper.classList.toggle("filter__top-wrapper--open");
-//   bottom_wrapper.classList.toggle("filter__bottom-wrapper--show");
-// });
-
+if (open_btn) {
+  open_btn.addEventListener('click', evt => {
+    evt.preventDefault();
+    top_wrapper.classList.toggle("filter__top-wrapper--open");
+    bottom_wrapper.classList.toggle("filter__bottom-wrapper--show");
+  });
+}
 //index
 
 const menu_btn = document.querySelector(".page-header__menu-btn");
 const menu_wrapper = document.querySelector(".page-header__menu-wrapper");
 
-menu_btn.addEventListener('click', evt => {
-  evt.preventDefault();
-  menu_wrapper.classList.toggle("page-header__menu-wrapper--show");
-});
+if (menu_btn) {
+  menu_btn.addEventListener('click', evt => {
+    evt.preventDefault();
+    menu_wrapper.classList.toggle("page-header__menu-wrapper--show");
+  });
+}
 
 const business_showbtn = document.querySelector(".rates__business-btn");
 const business_closebtn = document.querySelector(".business-rates__close-btn");
 const business_modal = document.querySelector(".business-rates");
 
-business_showbtn.addEventListener('click', evt => {
-  evt.preventDefault();
-  business_modal.classList.add("business-rates--show");
-});
+if (business_showbtn) {
+  business_showbtn.addEventListener('click', evt => {
+    evt.preventDefault();
+    business_modal.classList.add("business-rates--show");
+  });
+}
 
-business_closebtn.addEventListener('click', evt => {
-  evt.preventDefault();
-  business_modal.classList.remove("business-rates--show");
-});
+if (business_closebtn) {
+  business_closebtn.addEventListener('click', evt => {
+    evt.preventDefault();
+    business_modal.classList.remove("business-rates--show");
+  });
+}
